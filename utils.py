ff07b208912655d6d5e313748b54daba00f1aedd
liyumin <chn.lee.yumi@gmail.com>
2023-06-03 13:04:49 +0800

    代码注释优化

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import logging
 
 import numpy as np
 
-from config import *
+from config import LOG_LEVEL
 
 logging.basicConfig(level=LOG_LEVEL, format='%(asctime)s %(name)s %(levelname)s %(message)s')
 logger = logging.getLogger(__name__)
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 def get_file_hash(path):
     """
     计算文件hash
-    :param path: 文件路径
-    :return: 十六进制字符串
+    :param path: string, 文件路径
+    :return: string, 十六进制字符串
     """
     _hash = hashlib.sha1()
     try:
@@ -32,14 +32,20 @@ def get_file_hash(path):
 def get_string_hash(string):
     """
     计算字符串hash
-    :param string: 字符串
-    :return: 十六进制字符串
+    :param string: string, 字符串
+    :return: string, 十六进制字符串
     """
     _hash = hashlib.sha1()
     _hash.update(string.encode("utf8"))
     return _hash.hexdigest()
 
 
-def softmax(scores):
-    exp_scores = np.exp(scores)
+def softmax(x):
+    """
+    计算softmax，使得每一个元素的范围都在(0,1)之间，并且所有元素的和为1。
+    softmax其实还有个temperature参数，目前暂时不用。
+    :param x: [float]
+    :return: [float]
+    """
+    exp_scores = np.exp(x)
     return exp_scores / np.sum(exp_scores)
