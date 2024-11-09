545f7b49fd521fb4ef9dfe1a014467af8fcf287c
freemedom <57294686+freemedom@users.noreply.github.com>
2024-11-09 21:36:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,13 +11,16 @@ PORT = int(os.getenv('PORT', 8085))  # 监听端口
 
 # *****扫描配置*****
 # Windows系统的路径写法例子：'D:/照片'
-ASSETS_PATH = tuple(os.getenv('ASSETS_PATH', '/home,/srv').split(','))  # 素材所在的目录，绝对路径，逗号分隔
+ASSETS_PATH = tuple(os.getenv('ASSETS_PATH', 'G:/mi10ultra/DCIM/Camera.new,/srv').split(','))  # 素材所在的目录，绝对路径，逗号分隔 C:/Users/rui/Desktop/baiduapi/ImageSimilarity-main/9yuezhihou/testImageSimiHeic
 SKIP_PATH = tuple(os.getenv('SKIP_PATH', '/tmp').split(','))  # 跳过扫描的目录，绝对路径，逗号分隔
 IMAGE_EXTENSIONS = tuple(os.getenv('IMAGE_EXTENSIONS', '.jpg,.jpeg,.png,.gif,.heic,.webp,.bmp').split(','))  # 支持的图片拓展名，逗号分隔，请填小写
-VIDEO_EXTENSIONS = tuple(os.getenv('VIDEO_EXTENSIONS', '.mp4,.flv,.mov,.mkv,.webm,.avi').split(','))  # 支持的视频拓展名，逗号分隔，请填小写
+VIDEO_EXTENSIONS = tuple(os.getenv('VIDEO_EXTENSIONS', '.flv,.mov,.mkv,.webm,.avi').split(','))  # 支持的视频拓展名，逗号分隔，请填小写 .mp4,
 IGNORE_STRINGS = tuple(os.getenv('IGNORE_STRINGS', 'thumb,avatar,__MACOSX,icons,cache').lower().split(','))  # 如果路径或文件名包含这些字符串，就跳过，逗号分隔，不区分大小写
 FRAME_INTERVAL = int(os.getenv('FRAME_INTERVAL', 2))  # 视频每隔多少秒取一帧，视频展示的时候，间隔小于等于2倍FRAME_INTERVAL的算为同一个素材，同时开始时间和结束时间各延长0.5个FRAME_INTERVAL
-SCAN_PROCESS_BATCH_SIZE = int(os.getenv('SCAN_PROCESS_BATCH_SIZE', 8))  # 等读取的帧数到这个数量后再一次性输入到模型中进行批量计算，从而提高效率。显存较大可以调高这个值。
+SCAN_PROCESS_BATCH_SIZE = int(os.getenv('SCAN_PROCESS_BATCH_SIZE', 64))  # 等读取的帧数到这个数量后再一次性输入到模型中进行批量计算，从而提高效率。显存较大可以调高这个值。
+AUTOPROCESSOR_BATCH_SIZE = int(os.getenv('AUTOPROCESSOR_BATCH_SIZE', 4))  # 影响内存占用 AUTOPROCESSOR_BATCH_SIZE=4 num_workers=4 约增加0.5GB*4内存
+NUM_WORKERS = int(os.getenv('NUM_WORKERS', 4)) # 4进程并行，每个进程固定占用2.7GB内存
+
 IMAGE_MIN_WIDTH = int(os.getenv('IMAGE_MIN_WIDTH', 64))  # 图片最小宽度，小于此宽度则忽略。不需要可以改成0。
 IMAGE_MIN_HEIGHT = int(os.getenv('IMAGE_MIN_HEIGHT', 64))  # 图片最小高度，小于此高度则忽略。不需要可以改成0。
 AUTO_SCAN = os.getenv('AUTO_SCAN', 'False').lower() == 'true'  # 是否自动扫描，如果开启，则会在指定时间内进行扫描，每天只会扫描一次
@@ -28,8 +31,8 @@ AUTO_SAVE_INTERVAL = int(os.getenv('AUTO_SAVE_INTERVAL', 100))  # 扫描自动�
 # *****模型配置*****
 # 更换模型需要删库重新扫描！否则搜索会报错。数据库路径见下面SQLALCHEMY_DATABASE_URL参数。模型越大，扫描速度越慢，且占用的内存和显存越大。
 # 如果显存较小且用了较大的模型，并在扫描的时候出现了"CUDA out of memory"，请换成较小的模型。如果显存充足，可以调大上面的SCAN_PROCESS_BATCH_SIZE来提高扫描速度。
-# 4G显存推荐参数：小模型，SCAN_PROCESS_BATCH_SIZE=16
-# 8G显存推荐参数：小模型，SCAN_PROCESS_BATCH_SIZE=32
+# 4G显存推荐参数：小模型，SCAN_PROCESS_BATCH_SIZE=16 
+# 8G显存推荐参数：小模型，SCAN_PROCESS_BATCH_SIZE=32 无论使用cpu或cuda，均会占用内存大约8G
 # 超大模型最低显存要求是6G，且SCAN_PROCESS_BATCH_SIZE=1
 # 其余显存大小请自行摸索搭配。
 # 中文小模型： "OFA-Sys/chinese-clip-vit-base-patch16"
@@ -38,7 +41,7 @@ AUTO_SAVE_INTERVAL = int(os.getenv('AUTO_SAVE_INTERVAL', 100))  # 扫描自动�
 # 英文小模型： "openai/clip-vit-base-patch16"
 # 英文大模型："openai/clip-vit-large-patch14-336"
 MODEL_NAME = os.getenv('MODEL_NAME', "OFA-Sys/chinese-clip-vit-base-patch16")  # CLIP模型
-DEVICE = os.getenv('DEVICE', 'cpu')  # 推理设备，cpu/cuda/mps，建议先跑benchmark.py看看cpu还是显卡速度更快。因为数据搬运也需要时间，所以不一定是GPU更快。
+DEVICE = os.getenv('DEVICE', 'cuda')  # 推理设备，cpu/cuda/mps，建议先跑benchmark.py看看cpu还是显卡速度更快。因为数据搬运也需要时间，所以不一定是GPU更快。
 
 # *****搜索配置*****
 CACHE_SIZE = int(os.getenv('CACHE_SIZE', 64))  # 搜索缓存条目数量，表示缓存最近的n次搜索结果，0表示不缓存。缓存保存在内存中。图片搜索和视频搜索分开缓存。重启程序或扫描完成会清空缓存，或前端点击清空缓存（前端按钮已隐藏）。
