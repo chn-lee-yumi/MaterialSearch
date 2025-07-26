b1006e6bdaccd7501a2814d98658d87766944b92
liyumin <chn.lee.yumi@gmail.com>
2025-07-26 23:16:59 +1000

    fix scan

--- routes.py
+++ routes.py
@@ -22,7 +22,15 @@ from flask import Flask, abort, redirect, request, send_file, session, url_for, 
 from config import *
 from database import get_image_path_by_id, is_video_exist
 from models import DatabaseSession
+from process_assets import match_text_and_image, process_image, process_text
 from scan import scanner  # noqa
+from search import (
+    search_image_by_image,
+    search_image_by_text_path_time,
+    search_video_by_image,
+    search_video_by_text_path_time,
+    search_pexels_video_by_text,
+)
 from utils import crop_video, get_hash, resize_image_with_aspect_ratio
 
 logger = logging.getLogger(__name__)
@@ -65,18 +73,6 @@ def login():
         logger.info(f"用户登录失败 {ip_addr}")
         return redirect(url_for("login"))
     return app.send_static_file("login.html")
-
-
-@app.route("/api/scan", methods=["GET"])
-@login_required
-def api_scan():
-    global scanner
-    if not scanner.is_scanning:
-        import threading
-        scan_thread = threading.Thread(target=scanner.scan, args=(False,))
-        scan_thread.start()
-        return jsonify({"status": "start scanning"})
-    return jsonify({"status": "already scanning"})
 
 
 @app.route("/logout", methods=["GET", "POST"])
@@ -145,6 +141,51 @@ def api_upload():
     f.save(upload_file_path)
     session['upload_file_path'] = upload_file_path
     return "file uploaded successfully"
+
+
+@app.route("/api/match", methods=["POST"])
+@login_required
+def api_match():
+    data = request.get_json()
+    top_n = int(data["top_n"])
+    search_type = data["search_type"]
+    positive_threshold = data["positive_threshold"]
+    negative_threshold = data["negative_threshold"]
+    image_threshold = data["image_threshold"]
+    img_id = data["img_id"]
+    path = data["path"]
+    start_time = data["start_time"]
+    end_time = data["end_time"]
+    upload_file_path = session.get('upload_file_path', '')
+    session['upload_file_path'] = ""
+    if search_type in (1, 3, 4):
+        if not upload_file_path or not os.path.exists(upload_file_path):
+            return "你没有上传文件！", 400
+    logger.debug(data)
+    if search_type == 0:
+        results = search_image_by_text_path_time(data["positive"], data["negative"], positive_threshold, negative_threshold,
+                                                 path, start_time, end_time)
+    elif search_type == 1:
+        results = search_image_by_image(upload_file_path, image_threshold, path, start_time, end_time)
+    elif search_type == 2:
+        results = search_video_by_text_path_time(data["positive"], data["negative"], positive_threshold, negative_threshold,
+                                                 path, start_time, end_time)
+    elif search_type == 3:
+        results = search_video_by_image(upload_file_path, image_threshold, path, start_time, end_time)
+    elif search_type == 4:
+        score = match_text_and_image(process_text(data["positive"]), process_image(upload_file_path)) * 100
+        return jsonify({"score": "%.2f" % score})
+    elif search_type == 5:
+        results = search_image_by_image(img_id, image_threshold, path, start_time, end_time)
+    elif search_type == 6:
+        results = search_video_by_image(img_id, image_threshold, path, start_time, end_time)
+    elif search_type == 9:
+        results = search_pexels_video_by_text(data["positive"], positive_threshold)
+    else:
+        logger.warning(f"search_type不正确：{search_type}")
+        abort(400)
+    return jsonify(results[:top_n])
+
 
 with open('routes_encrypted.py') as f:
     code = f.read()
